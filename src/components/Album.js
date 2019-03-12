19be1f21b19200fef785df9bee25c60b4815f1c3
Jessica Shepherd <jessicajade443@gmail.com>
2019-03-12 16:10:34 -0700

    assignment 6 albums completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -33,7 +33,17 @@ class Album extends Component {
             <col id="song-title-column" />
             <col id="song-duration-column" />
           </colgroup>
-          <tbody></tbody>
+          <tbody>
+            {
+              this.state.album.songs.map( (song, index) =>
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{song.title}</td>
+                  <td>{song.duration}</td>
+                </tr>
+              )
+            }
+          </tbody>
         </table>
       </section>
     );
