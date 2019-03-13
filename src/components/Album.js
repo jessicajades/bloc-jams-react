25f44aaa5055d2bef7fcc02c5ff3c0430ff773d7
Jessica Shepherd <jessicajade443@gmail.com>
2019-03-13 14:48:44 -0700

    checkpoint 8 buttons completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar'
 
 
 class Album extends Component {
@@ -47,16 +48,26 @@ class Album extends Component {
     }
   }
 
+  handlePrevClick(){
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.max(0, currentIndex - 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+  }
+
   onHover(index){
     this.setState({isHovered: index});
   }
 
 
   hoverDisplay(song, index) {
-    if(index === this.state.isHovered) {
-      return <span className="icon ion-md-play"></span>
-    } else if(song === this.state.currentSong && this.state.isPlaying) {
+    if(song === this.state.currentSong && this.state.isPlaying) {
       return <span className="icon ion-md-pause"></span>
+    } else if(song === this.state.currentSong && this.state.isPlaying === false){
+      return <span className="icon ion-md-play"></span>
+    } else if(index === this.state.isHovered) {
+      return <span className="icon ion-md-play"></span>
     } else {
       return index + 1;
     }
@@ -97,6 +108,12 @@ class Album extends Component {
             }
           </tbody>
         </table>
+        <PlayerBar
+          isPlaying={this.state.isPlaying}
+          currentSong={this.state.currentSong}
+          handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+          handlePrevClick={() => this.handlePrevClick()}
+        />
       </section>
     );
   }
